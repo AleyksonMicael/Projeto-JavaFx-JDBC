aba1958beab3f5200ead414bda9257bb2bf9e255
Aleykson Micael Sampaio De Andrade <aleyksonmicael@gmail.com>
2022-08-24 17:19:09 -0300

    criando DepartamentView

--- src/main/java/com/example/javafxjdbc/MainViewController.java
+++ src/main/java/com/example/javafxjdbc/MainViewController.java
@@ -32,7 +32,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     protected void onMenuItemDepartmentAction() {
-        System.out.println("onMenuItemDepartmentAction");
+        loadView("/gui/DepartmentList.fxml");
     }
 
     @FXML
